df26dc85e0cab3ac8aed1a8d72dd14c10b1ddcc5
Kelsey Velez <kelsey@Kelseys-MBP.lan>
2024-09-12 12:28:05 -0400

    added Module 3 Assignment - Data Frames

---
+++ LIS 6371 Module 3 Assignment.R
@@ -0,0 +1,15 @@
+#assign data to variables
+Jeb <- c(4, 12)
+Donald <- c(62, 75)
+Ted <- c(51, 43)
+Marco <- c(21, 19)
+Carly <- c(2, 1)
+Hillary <- c(14, 21)
+Bernie <- c(15, 19)
+#create data frame 
+results <- data.frame(Jeb, Donald, Ted, Marco, Carly, Hillary, Bernie, stringsAsFactors = FALSE, row.names = c("ABC", "CBS"))
+#print results
+results
+
+
+
