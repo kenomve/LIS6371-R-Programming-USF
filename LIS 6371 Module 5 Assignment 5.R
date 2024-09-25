eff409d51c066f09e7c06f399aa4ba726f2c373e
Kelsey Velez <kelsey@Kelseys-MBP.lan>
2024-09-25 14:08:22 -0400

    Added Assignment 5 - Matrices

---
+++ LIS 6371 Module 5 Assignment 5.R
@@ -0,0 +1,11 @@
+#Find the determinant and inverse of Matrix A annd B
+A = matrix(1:100, nrow=10)
+B = matrix(1:1000, nrow=10)
+
+#determinant for Matrix A
+det(A)
+#determinant for Matrix A is 0
+#there is no inverse for matrix A because the determinant is 0
+
+#determinant for matrix B does not exist because the matrix is not square
+#there is no inverse for matrix B because there is no determinant
